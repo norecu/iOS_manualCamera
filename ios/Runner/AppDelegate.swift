c1d1299007c027873e786870ca5d03fd0dc869dc
norecu <1000g.csec@gmail.com>
2026-09-06 15:54:13 +0900

    feat: Add Swift-based Camera | Add iOS Github Actions build

--- ios/Runner/AppDelegate.swift
+++ ios/Runner/AppDelegate.swift
@@ -2,15 +2,40 @@ import Flutter
 import UIKit
 
 @main
-@objc class AppDelegate: FlutterAppDelegate, FlutterImplicitEngineDelegate {
-  override func application(
-    _ application: UIApplication,
-    didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?
-  ) -> Bool {
-    return super.application(application, didFinishLaunchingWithOptions: launchOptions)
-  }
+@objc class AppDelegate: FlutterAppDelegate {
 
-  func didInitializeImplicitFlutterEngine(_ engineBridge: FlutterImplicitEngineBridge) {
-    GeneratedPluginRegistrant.register(with: engineBridge.pluginRegistry)
-  }
+    let cameraEngine = CameraEngine()
+
+    override func application(
+        _ application: UIApplication,
+        didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?
+    ) -> Bool {
+
+        GeneratedPluginRegistrant.register(with: self)
+
+        let controller = window?.rootViewController as! FlutterViewController
+
+        let factory = CameraPlatformViewFactory(
+            cameraEngine: cameraEngine
+        )
+
+        controller.registrar(
+            forPlugin: "CameraPlatformView"
+        ).register(
+            factory,
+            withId: "ios-camera-preview"
+        )
+
+        do {
+            try cameraEngine.setup()
+            cameraEngine.start()
+        } catch {
+            print("Camera setup error: \(error)")
+        }
+
+        return super.application(
+            application,
+            didFinishLaunchingWithOptions: launchOptions
+        )
+    }
 }
